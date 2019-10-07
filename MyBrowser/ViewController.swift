ee2b476c4ed4f8398dc9eca0216054cb82c1847d
Megat Syafiq <syafiqq95@gmail.com>
2019-10-07 22:19:20 +0800

    Initial Commit

---
+++ MyBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyBrowser
+//
+//  Created by Megat Syafiq on 07/10/2019.
+//  Copyright © 2019 Megat Syafiq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
